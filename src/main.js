bd3d8d12839c92564ceb335b9b6d022b8cce5510
Bryan McConnahea <transcendrstudio@gmail.com>
2018-08-18 04:17:08 +0200

    Implements _createRoot examples

--- src/main.js
+++ src/main.js
@@ -3,21 +3,56 @@ import {
     html
 } from '@polymer/lit-element';
 
-class TestComponent extends LitElement {
+class ComponentAPI extends LitElement {
 
+    /**
+     * Setup your component properties whose values are passed into the
+     * component via it's respective attributes.  For example:
+     * <my-clock format="12"></my-clock>
+     */
     static get properties() {
         return {
-            mood: String
+            format: String
         }
     }
 
+    /**
+     * By using a constructor (which is optional), you must call super
+     * before anything else.
+     */
+    constructor() {
+        super();
+    }
+
+    /**
+     * Implement to customize where the element's template is rendered by returning 
+     * an element into which to render. By default this creates a shadowRoot for 
+     * the element. To render into the element's childNodes, return this.
+     * https://developer.mozilla.org/en-US/docs/Web/API/Element/attachShadow
+     */
+    _createRoot() {
+        //Example: the default behavior is to create a shadowRoot in open mode
+        //If this is your use case, you can simply not implement _createRoot
+        return this.attachShadow({
+            mode: 'open'
+        });
+
+        //Example: to render without creating a shadowRoot, return this instead
+        // return this;
+
+        //Example: to render the component into a specific DOM element
+        // return document.getElementById('my-clock-root');
+    }
+
     _render({
-        mood
+        format
     }) {
-        return html `<style> .mood { color: green; } </style>
-        Web Components are <span class="mood">${mood}</span>!`;
+        return html `
+            <style> .format { color: #ff3333; } </style>
+            <h1>JS Clock in <span class="format">${format} Hour</span> Format!</h1>
+        `;
     }
 
 }
 
-customElements.define('test-component', TestComponent);
+customElements.define('my-clock', ComponentAPI);
